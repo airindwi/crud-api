1d43e1e77c7a5d65ade836efb35731b8c2bd8ed5
airindwi <airindwi1503@gmail.com>
2023-01-30 16:12:07 +0700

    title

---
+++ app/Http/Controllers/TitleController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Title;
+use Illuminate\Http\Request;
+
+class TitleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $titles = Title::paginate(10);
+        return response()->json([
+            'data' => $titles
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $title = Title::create([
+            'emp_no' => $request->emp_no,
+            'title' => $request->title,
+            'from_date' => $request->from_date,
+            'to_date' => $request->to_date,
+        ]);
+
+        return response()->json([
+            'data' => $title
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Title  $title
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Title $title)
+    {
+        return response()->json([
+            'data' => $title
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Title  $title
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Title $title)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Title  $title
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Title $title)
+    {
+
+        $title->update($request->all());
+
+        return response()->json([
+            'data' => $title
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Title  $title
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Title $title)
+    {
+       $title->delete();
+        return response()->json([
+            'message' => 'title deleted'
+        ], 204);
+    } 
+}
